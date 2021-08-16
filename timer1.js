d195b8c542e855fabbefb674ce9eb46a5303ddd0
TJ-Blinn <tjblinn@hotmail.com>
2021-08-16 20:15:32 +0000

    fixed readout to bypass noise

---
+++ timer1.js
@@ -0,0 +1,39 @@
+/*
+cmd line arguments
+* node timer1.js 10 3 5 15 9
+
+    * No numbers are provided: Easy. It should just not beep at all and end immediately
+since no beeps should get scheduled.
+    * An input is a negative number: Ignore/skip any numbers that are negative.
+We can't schedule anything in the past.
+    * An input is not a number: Ignore/skip these as well, instead of attempting to call
+setTimeout with a non-number.
+
+Pseudocode
+- process.argv.slice(2)
+*/
+// ************************************
+// "use strict";
+
+const timer = function (time) {
+  const cmdInput = process.argv.slice(2);
+
+  if (cmdInput === " " && cmdInput <= 0 && isNaN(time)) {
+    return null;
+  }
+
+  //iterates over array(only)
+  cmdInput.forEach((time) => {
+    setTimeout(() => {
+      process.stdout.write("noise\n");
+    }, time * 1000);
+  });
+
+  // const alertTerminal = function() {
+  //   console.log("\x07");
+  // console.log("\u0007");
+  // };
+};
+
+timer();
+// process.argv for this to run | node timer1.js 5
